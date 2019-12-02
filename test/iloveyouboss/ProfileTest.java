eacaa06a04a1099df4090fc799dbdc7b7458ad26
binaryyoung <asqw4545@gmail.com>
2019-12-02 16:25:58 +0900

    criterion의 weight가 Dontcare인 경우 True를 반환하는 테스트 코드 작성

--- test/iloveyouboss/ProfileTest.java
+++ test/iloveyouboss/ProfileTest.java
@@ -1,6 +1,7 @@
 package iloveyouboss;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -10,7 +11,7 @@ import iloveyouboss.Question;
 public class ProfileTest {
 
 	/**
-	 * MustMatch인 질문의 답변이 서로 다른 경우
+	 * 가중치가 MustMatch인 질문의 답변이 서로 다른 경우
 	 */
 	@Test
 	public void matchAnswerFalseWhenMustMatchCriteriaNotMet() {
@@ -24,8 +25,27 @@ public class ProfileTest {
 		criteria.add(criterion);
 		
 		boolean matches = profile.matches(criteria);
-		
+			
 		assertFalse(matches);
+	}
+
+	/**
+	 * 가중치가 DontCare일 때 True를 반환하는지 테스트
+	 */
+	@Test
+	public void matchAnswerTrueForAnyDontCareCriteria() {
+		Profile profile = new Profile("Bull Hockey, Inc.");
+		Question question = new BooleanQuestion(1, "Got Milk?");
+		Answer profileAnswer = new Answer(question, Bool.FALSE);
+		profile.add(profileAnswer);
+		Criteria criteria = new Criteria();
+		Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+		Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+		criteria.add(criterion);
+		
+		boolean matches = profile.matches(criteria);
+		
+		assertTrue(matches);
 	}
 
 }
